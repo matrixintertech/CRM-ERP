2ff364d6e79b05f750487d2f1fc5fd5f42de5f21
matrix-intertech <jbsinghnarang@gmail.com>
2026-08-08 12:20:41 +0530

    feat: add TanStack Query provider and configuration

--- admin/src/main.tsx
+++ admin/src/main.tsx
@@ -1,24 +1,62 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+
+import {
+  BrowserRouter,
+} from "react-router-dom";
+
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
+import {
+  Toaster,
+} from "sonner";
+
 import "@/styles/globals.css";
-import { Toaster } from "sonner";
+
 import App from "./App";
-import { AuthProvider } from "./app/providers/AuthProvider";
+
+import {
+  AuthProvider,
+} from "./app/providers/AuthProvider";
+
+const queryClient =
+  new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime:
+          60 * 1000,
+
+        refetchOnWindowFocus:
+          false,
+
+        retry: 1,
+      },
+    },
+  });
 
 ReactDOM.createRoot(
-  document.getElementById("root")!,
+  document.getElementById(
+    "root",
+  )!,
 ).render(
   <React.StrictMode>
-    <BrowserRouter>
-    <AuthProvider>
-      <App />
-       <Toaster
-          richColors
-          position="top-right"
-          closeButton
-        />
-      </AuthProvider>
-    </BrowserRouter>
+    <QueryClientProvider
+      client={queryClient}
+    >
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+
+          <Toaster
+            richColors
+            position="top-right"
+            closeButton
+          />
+        </AuthProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
