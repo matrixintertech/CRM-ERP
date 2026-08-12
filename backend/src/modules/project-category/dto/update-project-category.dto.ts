ce5f24350d50907a70aa60f653eeaf25f76a70c2
matrix-intertech <jbsinghnarang@gmail.com>
2026-08-12 11:50:29 +0530

    fix: allow project category status updates

--- backend/src/modules/project-category/dto/update-project-category.dto.ts
+++ backend/src/modules/project-category/dto/update-project-category.dto.ts
@@ -1,13 +1,29 @@
 import {
+  ApiPropertyOptional,
   PartialType,
-} from "@nestjs/swagger";
+} from '@nestjs/swagger';
+
+import {
+  IsEnum,
+  IsOptional,
+} from 'class-validator';
+
+import {
+  Status,
+} from '@prisma/client';
 
 import {
   CreateProjectCategoryDto,
-} from "./create-project-category.dto";
+} from './create-project-category.dto';
 
-
-export class UpdateProjectCategoryDto
-extends PartialType(
+export class UpdateProjectCategoryDto extends PartialType(
   CreateProjectCategoryDto,
-) {}
+) {
+  @ApiPropertyOptional({
+    enum: Status,
+    example: Status.ACTIVE,
+  })
+  @IsOptional()
+  @IsEnum(Status)
+  status?: Status;
+}
